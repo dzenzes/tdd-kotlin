ad20480da3462177ebfe524dfa46232d2cabe58f
Daniel Zenzes <zenzes@bryter.io>
2022-07-08 08:17:06 +0200

    add junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.+")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.+")
 }
